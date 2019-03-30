9bd48842d1e4ea750b87812ec877f7973b5efe4b
Felipe <claiohm@gmail.com>
2019-03-30 20:22:24 -0300

    Creating initial structure

---
+++ src/main/java/br/com/felipe/renegociacao/RenegociacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.renegociacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenegociacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenegociacaoApplication.class, args);
+    }
+
+}
